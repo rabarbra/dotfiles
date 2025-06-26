34d184a7efe79da34f0b15a29829557904409908
Polina Simonenko <rabarbrablad@gmail.com>
2025-06-26 16:44:45 +0200

    wip
    Signed-off-by: Polina Simonenko <rabarbrablad@gmail.com>

--- os/macos/install.sh
+++ os/macos/install.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
@@ -8,14 +9,13 @@ WHITE='\033[0;37m'
 BOLD='\033[1m'
 NC='\033[0m'
 
-
-#!/bin/bash
 xcode-select --install
 
 # Clone the dotfiles repository if it doesn't exist
-DOTFILES_DIR="$HOME/.dotfiles"
+# DOTFILES_DIR="$HOME/.dotfiles"
+DOTFILES_DIR="."
 if [ ! -d "$DOTFILES_DIR" ]; then
-    git clone https://github.com/rabarbra/dotfiles.git "$DOTFILES_DIR"
+    git clone git@github.com:rabarbra/dotfiles.git "$DOTFILES_DIR"
     if [ $? -ne 0 ]; then
         printf "${RED}Failed to clone the dotfiles repository.${NC}\n"
         exit 1
@@ -37,5 +37,22 @@ else
     printf "${YELLOW}Homebrew is already installed.${NC}\n"
 fi
 
+# Install packages from Brewfile
+brew bundle --file=${DOTFILES_DIR}/os/macos/Brewfile
+if [ $? -ne 0 ]; then
+    printf "${RED}Failed to install packages from Brewfile.${NC}\n"
+    exit 1
+fi
+printf "${GREEN}Packages installed successfully from Brewfile.${NC}\n"
 
-# brew bundle --file=$(DOTFILES_DIR)/macos/Brewfile
+# Install Oh My Zsh
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    if [ $? -ne 0 ]; then
+        printf "${RED}Oh My Zsh installation failed.${NC}\n"
+        exit 1
+    fi
+    printf "${GREEN}Oh My Zsh installed successfully.${NC}\n"
+else
+    printf "${YELLOW}Oh My Zsh is already installed.${NC}\n"
+fi
